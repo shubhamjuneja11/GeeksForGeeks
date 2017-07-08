d0692a65dfa30167893b5ff40fcb959e73fa17e9
Shubham <shubhamjuneja11@gmail.com>
2017-07-08 21:19:34 +0530

    first commit

---
+++ LinkedList.java
@@ -0,0 +1,16 @@
+class LinkedList{
+Node head;
+	class Node{
+		int data;
+		Node next;
+
+		Node(int d){
+			this.data=data;
+			this.next=null;
+		}
+	}
+	public void push(int new_data){
+		Node node=new Node(new_data);
+		n
+	}
+}
